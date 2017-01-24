499a6553cc3c79759a9f43022026ed987bc729e4
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2017-01-24 20:30:44 +0100

    Remove task list

--- rc_api_v4.lua
+++ rc_api_v4.lua
@@ -178,9 +178,6 @@ awful.screen.connect_for_each_screen(function(s)
     -- Create a taglist widget
     s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, taglist_buttons)
 
-    -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklist_buttons)
-
     -- Create the wibox
     s.mywibox = awful.wibar({ position = "top", screen = s })
 
@@ -192,7 +189,9 @@ awful.screen.connect_for_each_screen(function(s)
             s.mytaglist,
             s.mypromptbox,
         },
-        s.mytasklist, -- Middle widget
+        { -- Middle widgets
+          layout = wibox.layout.fixed.horizontal
+        },
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
             mykeyboardlayout,
