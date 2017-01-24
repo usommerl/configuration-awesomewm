209c836647911bd7fd235d7d26113c414ee5183b
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2017-01-24 21:16:03 +0100

    Add size_hints_honor rule for urxvt

--- rc_api_v4.lua
+++ rc_api_v4.lua
@@ -445,10 +445,8 @@ awful.rules.rules = {
     { rule_any = {type = { "normal", "dialog" }
       }, properties = { titlebars_enabled = false }
     },
-
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
+    { rule = { class = "URxvt" },
+      properties = { size_hints_honor = false } },
 }
 -- }}}
 
