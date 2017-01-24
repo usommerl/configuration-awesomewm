47db8a721553ecab4371aa85443b2bf04dd877ea
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2017-01-24 20:16:46 +0100

    Set standard editor and terminal

--- rc_api_v4.lua
+++ rc_api_v4.lua
@@ -41,8 +41,8 @@ end
 beautiful.init(awful.util.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
-editor = os.getenv("EDITOR") or "nano"
+terminal = "urxvt"
+editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
