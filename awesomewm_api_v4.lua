542816f49d121320a8b2d659857b3e835d44cccf
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2018-10-15 21:17:15 +0200

    Change lockscreen shortcut

--- awesomewm_api_v4.lua
+++ awesomewm_api_v4.lua
@@ -486,7 +486,7 @@ globalkeys = awful.util.table.join(
   awful.key({ modkey                         }, "+",          function () awful.spawn.with_shell("amixer --quiet set Master 1%+")            end),
   awful.key({ modkey                         }, "b",          function () mouse.screen.mywibox.visible = not mouse.screen.mywibox.visible    end),
   awful.key({ modkey                         }, "F7",         function () awful.spawn.with_shell("sleep 1; xset s activate")                 end),
-  awful.key({ modkey                         }, "Pause",      function () awful.spawn.with_shell("i3lock -c 000000")                         end),
+  awful.key({ modkey                         }, "End",        function () awful.spawn.with_shell("i3lock -c 000000")                         end),
   awful.key({ modkey                         }, "p",          function () awful.spawn.with_shell("ncmpcpp toggle")                           end),
   awful.key({ modkey, "Shift", "Control"     }, "s",          function () awful.spawn.with_shell("poweroff")                                 end),
   awful.key({ modkey,                        }, "r",          function ()
