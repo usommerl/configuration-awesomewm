51c5898975dc7e0faaf63e7ba21dfe193aa32c99
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2020-12-04 15:48:34 +0100

    Change keybinding to toggle sticky client

--- awesomewm_api_v4.lua
+++ awesomewm_api_v4.lua
@@ -544,7 +544,7 @@ clientkeys = awful.util.table.join(
   awful.key({ modkey, "Shift"                }, "c",          function (c) c:kill()                                                          end),
   awful.key({ modkey, "Control"              }, "Return",     function (c) c:swap(awful.client.getmaster())                                  end),
   awful.key({ modkey,                        }, "o",          function (c) c:move_to_screen()                                                end),
-  awful.key({ modkey,                        }, "t",          function (c) c.ontop = not c.ontop                                             end),
+  awful.key({ modkey,                        }, "t",          function (c) c.sticky = not c.sticky                                           end),
   awful.key({ modkey,                        }, "n",          function (c) c.minimized = true                                                end),
   awful.key({ modkey,                        }, "m",          function (c)
                                                                   c.maximized = not c.maximized
