1bbab9c378a65d70b9cab4ad10c9fb987c3c2e39
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2021-03-25 14:46:41 +0100

    Add a half-arsed dracula theme

--- awesomewm_api_v4.lua
+++ awesomewm_api_v4.lua
@@ -254,7 +254,7 @@ end
 -- }}}
 
 -- {{{ Variable definitions
-beautiful.init(awful.util.getdir("config") .. "/themes/zenburn-mod/theme.lua")
+beautiful.init(awful.util.getdir("config") .. "/themes/dracula/theme.lua")
 terminal = "alacritty"
 rofiRunCommand = "rofi -no-disable-history -combi-modi window,run,ssh -show combi -modi combi"
 rofiClipboardCommand = [[
