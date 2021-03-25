51df61608903f28f5f29649a5534a5926e351e77
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2021-03-25 14:36:25 +0100

    Revert "Change focus color"
    This reverts commit d70bdf0c7942e102f6b6539d9c3cbf5a44f15b1c.

--- themes/zenburn-mod/theme.lua
+++ themes/zenburn-mod/theme.lua
@@ -11,7 +11,7 @@ theme.bg_focus  = "#444444"
 theme.bg_urgent = "#CC0000"
 
 theme.border_normal = "#000000"
-theme.border_focus  = "#80a8dc"
+theme.border_focus  = "#ffcb6b"
 theme.border_width  = "1"
 
 theme.taglist_squares_sel   = awful.util.getdir("config") .. "/themes/zenburn-mod/taglist/squarefz.png"
